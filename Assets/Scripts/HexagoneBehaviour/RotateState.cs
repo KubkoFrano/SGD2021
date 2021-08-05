49b48a389474dde3c82e8c19133b3a124bd2e525
m1.r0 <myroslav.kliestik@gmail.com>
2021-08-05 11:31:17 +0200

    FIX: water shader, random platform texture

--- Assets/Scripts/HexagoneBehaviour/RotateState.cs
+++ Assets/Scripts/HexagoneBehaviour/RotateState.cs
@@ -11,7 +11,7 @@ public class RotateState : State
     public override State Execute()
     {
         Quaternion target = Quaternion.Euler(0, 0, 0);
-        data.transform.Rotate(0, 0, data.direction * Time.deltaTime * 20);
+        data.transform.Rotate(0, data.direction * Time.deltaTime * 20, 0);
 
         if (data.transform.localEulerAngles.y >= data.lastRotationZ + data.rotateItteration * 60 || data.transform.localEulerAngles.y <= data.lastRotationZ - data.rotateItteration * 60)  // fakt dlha podmienka XD sry
         {
