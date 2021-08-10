af7ac27c6209533aaff3d7e66991ad045170fa93
KubkoFrano <franojakub@gmail.com>
2021-08-10 10:05:02 +0200

    FIX: movement on respawn

--- Assets/Scripts/Player/RespawnBehaviour.cs
+++ Assets/Scripts/Player/RespawnBehaviour.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class RespawnBehaviour : MonoBehaviour
 {
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Water"))
         {
             transform.position = App.playerManager.GetSpawnPositions()[Random.Range(0, 4)];
+            rb.velocity = Vector3.zero;
         }
     }
 }
