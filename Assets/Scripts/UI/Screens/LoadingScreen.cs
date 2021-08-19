c16d11b35bd984c1e22e7b70be32761ccf83bc81
KubkoFrano <franojakub@gmail.com>
2021-08-19 10:27:28 +0200

    ADD: loading screen

---
+++ Assets/Scripts/UI/Screens/LoadingScreen.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : ScreenBase
+{
+    [SerializeField] float timeBetweenScreens;
+    [SerializeField] Sprite[] sprites;
+
+    Image image;
+
+    private void Awake()
+    {
+        image = GetComponentInChildren<Image>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(Loading());
+    }
+
+    IEnumerator Loading()
+    {
+        foreach (Sprite sprite in sprites)
+        {
+            image.sprite = sprite;
+            yield return new WaitForSeconds(timeBetweenScreens);
+        }
+
+        Time.timeScale = 1;
+        App.gameManager.StartSceneLoading(App.gameManager.GetLevelScene());
+        App.screenManager.Show<InGameScreen>();
+        App.playerManager.InitPlayers();
+        App.playerManager.SetupCameras();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        App.timer.StartTimer();
+        App.inGameScreen.ResetScores();
+        App.inGameScreen.SetPlayerNumber(App.playerManager.GetPlayerCount());
+        App.playerManager.CreatePlayerTransforms();
+        App.playerManager.CreatePlayerScores();
+        App.gameManager.SetGameState(GameState.game);
+        App.inGameScreen.ResetBirds();
+        Hide();
+    }
+}
--- Assets/Scripts/UI/Screens/LobbyScreen.cs
+++ Assets/Scripts/UI/Screens/LobbyScreen.cs
@@ -29,20 +29,7 @@ public class LobbyScreen : ScreenBase
         if (!(App.playerManager.GetPlayerCount() == 2 || App.playerManager.GetPlayerCount() == 4))
             return;
 
-        Time.timeScale = 1;
-        App.gameManager.StartSceneLoading(App.gameManager.GetLevelScene());
-        App.screenManager.Show<InGameScreen>();
-        App.playerManager.InitPlayers();
-        App.playerManager.SetupCameras();
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        App.timer.StartTimer();
-        App.inGameScreen.ResetScores();
-        App.inGameScreen.SetPlayerNumber(App.playerManager.GetPlayerCount());
-        App.playerManager.CreatePlayerTransforms();
-        App.playerManager.CreatePlayerScores();
-        App.gameManager.SetGameState(GameState.game);
-        App.inGameScreen.ResetBirds();
+        App.screenManager.Show<LoadingScreen>();
         Hide();
     }
 }
