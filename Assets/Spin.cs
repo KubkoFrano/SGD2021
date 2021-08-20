d02b5a58539aae2f1ef431f0e0af09112352ceb9
m1.r0 <myroslav.kliestik@gmail.com>
2021-08-20 15:00:26 +0200

    FIX: PowerUPShaders

--- Assets/Spin.cs
+++ Assets/Spin.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
+    [SerializeField]
+    float spid = 1f;
+    [SerializeField]
+    float magnitude = 1f;
 
     void Update()
     {
-        this.transform.Translate(0, (Mathf.Sin(Time.time * 3f)/ 4) * Time.deltaTime, 0);
+        this.transform.Translate(0, (Mathf.Sin(Time.time * spid) * magnitude) * Time.deltaTime, 0);
         this.transform.Rotate(0, 0.5f, 0);
     }
 }
