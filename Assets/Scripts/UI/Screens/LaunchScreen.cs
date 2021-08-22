820467c04950ccd710a904aaaaf44ed6a6f82f67
KubkoFrano <franojakub@gmail.com>
2021-08-22 18:45:44 +0200

    ADD: launch screen backend

--- Assets/Scripts/UI/Screens/LaunchScreen.cs
+++ Assets/Scripts/UI/Screens/LaunchScreen.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class LaunchScreen : ScreenBase
-{ 
+{
+    [SerializeField] VideoPlayer videoPlayer;
+
     public void Launch()
     {
+        videoPlayer.gameObject.SetActive(true);
+        videoPlayer.Play();
+
+        StartCoroutine(DelayLaunch());
+    }
+
+    IEnumerator DelayLaunch()
+    {
+        yield return new WaitForSeconds((float) videoPlayer.clip.length);
         App.screenManager.Show<MenuScreen>();
         Hide();
     }
