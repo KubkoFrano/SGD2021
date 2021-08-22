b020d79cd431a4ccd47bca9fdbffeac8d43270e1
KubkoFrano <franojakub@gmail.com>
2021-08-22 19:11:19 +0200

    ADD: repaired launch

--- Assets/Scripts/UI/Screens/LaunchScreen.cs
+++ Assets/Scripts/UI/Screens/LaunchScreen.cs
@@ -6,21 +6,8 @@ using UnityEngine.Video;
 
 public class LaunchScreen : ScreenBase
 {
-    [SerializeField] VideoPlayer videoPlayer;
-    [SerializeField] Image image;
-
     public void Launch()
     {
-        //image.enabled = false;
-        videoPlayer.gameObject.SetActive(true);
-        videoPlayer.Play();
-
-        StartCoroutine(DelayLaunch());
-    }
-
-    IEnumerator DelayLaunch()
-    {
-        yield return new WaitForSeconds((float) videoPlayer.clip.length);
         App.screenManager.Show<MenuScreen>();
         Hide();
     }
